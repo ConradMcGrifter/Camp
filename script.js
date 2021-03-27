aabcf1227ac2d123f1730f0b4b1c8af2399e8a0f
ConradMcGrifter <79630413+ConradMcGrifter@users.noreply.github.com>
2021-03-27 15:22:23 -0400

    changed some of the variables for madlibs

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function buildMadLib() {
     let tool = document.getElementById("tool").value;
     let adjective = document.getElementById("adjective").value;
     let place = document.getElementById("place").value;
-    let object = document.getElementById("object").value;
+    let noun = document.getElementById("noun").value;
     let food = document.getElementById("food").value;
-    let noun = document.getElementById("plural-noun").value;
+    let pluralNoun = document.getElementById("plural-noun").value;
 
     //process
     let result =
@@ -19,13 +19,13 @@ function buildMadLib() {
         adjective +
         " explorers came from " +
         place +
-        " with one goal...To make the best camp the world has ever seen. Equipped with only a couple fishing rods, a " +
+        " with one goal...To make the best camp the world has ever seen. Equipped with only a couple fishing rods, an axe, and a " +
         tool +
-        " and a " +
-        object +
         ", they were able to construct a small shelter and gather enough " +
         food +
-        " to live comfortably. Over the years the camp has only gotten stronger, and it is now responsible for the entire economy of Center Sandwich. If you are coming to visit the camp, meals will be provided but you must bring your own " +
+        " to live comfortably. Over the years the camp has only gotten stronger, and it is now responsible for the entire economy of Center Sandwich. If you are coming to visit the camp, " +
+        pluralNoun +
+        " will be provided but you must bring your own " +
         noun +
         ".";
     //output
