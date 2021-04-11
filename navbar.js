bee873182f0237b1b59bc1d13e99a0c838cb9b7b
sam <79630413+ConradMcGrifter@users.noreply.github.com>
2021-04-11 13:37:27 -0400

    made the hamburger rotate when clicked

--- navbar.js
+++ navbar.js
@@ -4,3 +4,11 @@ const navUl = document.getElementById("nav__ul");
 hamburger.addEventListener("click", () => {
     navUl.classList.toggle("show");
 });
+
+hamburger.addEventListener("click", animation);
+
+function animation() {
+    // hamburger.style.animation = "transitionIn 1s";.
+    hamburger.classList.toggle("rotate");
+    // hamburger.style.rotate = "90deg";
+}
