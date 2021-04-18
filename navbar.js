aaeff78a9b68493e83dd433f32ae9ef09f83f708
sam <79630413+ConradMcGrifter@users.noreply.github.com>
2021-04-18 16:30:39 -0400

    messing around with the animation for nav__ul
    trying to get it to repeat the animation for mobile

--- navbar.js
+++ navbar.js
@@ -1,19 +1,35 @@
 const hamburger = document.getElementById("hamburger");
 const navUl = document.getElementById("nav__ul");
 const darken = document.getElementById("darken");
+const showNav = document.getElementsByClassName("show");
 
 //toggle drop down menu
 hamburger.addEventListener("click", show);
 
 function show() {
-    // navUl.style.transform.translateY = "-20";
-    navUl.classList.toggle("show");
-    darken.classList.toggle("darken");
+    // navUl.classList.toggle("show");
+    // darken.classList.toggle("darken");
 
-    if (navUl.classList.contains("show")) {
+    // if (navUl.classList.contains("show")) {
+    //     navUl.style.animation = "shortTransitionIn 0.5s";
+    // }
+
+    // if (!navUl.classList.contains("show")) {
+    //     navUl.style.animation = "shortTransitionOut 0.5s";
+    // }
+    // if (!navUl.classList.contains("show")) {
+    //     navUl.style.transform = "translateY(-20px)";
+    // } else if (navUl.classList.contains("show")) {
+    //     navUl.style.transform = "translateY(0px)";
+    // }
+
+    if (!navUl.classList.contains("show")) {
+        navUl.style.transform = "translateY(0)";
+        navUl.classList.add("show");
         navUl.style.animation = "shortTransitionIn 0.5s";
-    } else {
-        navUl.style.animation = "";
+    } else if (navUl.classList.contains("show")) {
+        navUl.classList.remove("show");
+        navUl.style.transform = "translateY(-20px)";
     }
 }
 
